eceae1884f56a77bbf1afb64a6fc2b286796a961
Kunal Mehetre <kdmehetre2004@gmail.com>
2025-04-02 11:39:50 -0700

    new

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './Navbar.css'
-import {assets} from '../../assets/assets'
+
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <img className='logo' src={assets.logo} alt="" />
-      <img className='profile' src={assets.profile_image} alt="" />
+      <h2>Hunger Hub</h2>
+      <h4>Admin</h4>
     </div>
   )
 }
